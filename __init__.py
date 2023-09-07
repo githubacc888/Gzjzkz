0b9accaf32798025810981e478cff14b3697a64f
OscarPy <144183135+githubacc888@users.noreply.github.com>
2023-09-07 03:50:10 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .OscarPy import info
+from .OscarPy import email
+from .OscarPy import linked
+from .OscarPy import username
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name='OscarPy',
+    version='0.2',
+    author='OscarTools',
+    description='Easy time in OscarPy library',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
